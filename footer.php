6f244a9f261be39b4b0a334fc1d63bbb3c4d4c44
ruwandi <ruwandi1102@gmail.com>
2018-12-29 09:43:06 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color:black;
+   color: white;
+   text-align: center;
+}
+</style>
+</head>
+<body>
+
+
+<div class="footer">
+  <p>contact no: 03078654323 | Email: www.sab.ac.lk</p>
+</div>
+
+</body>
+</html> 
